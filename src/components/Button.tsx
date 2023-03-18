9a278c61b2debddc56610be7fe5983aac11d12da
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-03-18 20:19:18 +0530

    fix: build errs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,5 +2,5 @@ import type { FC } from 'react';
 import { Button } from '@chakra-ui/react';
 
 export const MyButton: FC = () => {
-  return <Button>I am a button as useless as Anurag</Button>;
+  return <Button>I am a button</Button>;
 };
